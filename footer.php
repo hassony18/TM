a2cd59423bcc7ef0716d99432690edd6ee8cdc7a
HassoN <56402041+hassony18@users.noreply.github.com>
2020-10-10 20:56:16 +0200

    s

--- footer.php
+++ footer.php
@@ -1,7 +1,7 @@
 <footer>
 	  <section id="footer">
 		<div class="footer container">
-		  <div class="brand"><h1><span>S</span>haif <span>A</span>rfan</h1></div>
+		  <div class="brand"><h1><span>S</span>wiss <span>L</span>earns</h1></div>
 		  <h2>Your Complete Web Solution</h2>
 		  <div class="social-icon">
 			<div class="social-item">
@@ -21,7 +21,7 @@
 		</div>
 	  </section>
 </footer>
-
+	<script src="./app.js"></script>
 	
 
 </body>
