332afb62f9fa1dc58cc621c693e596f643885730
HassoN <56402041+hassony18@users.noreply.github.com>
2020-10-10 21:09:37 +0200

    s

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
-const hamburger = document.querySelector('.header .nav-bar .nav-list .hamburger');
+const headerList = document.querySelector('.header .nav-bar .nav-list .headerList');
 const mobile_menu = document.querySelector('.header .nav-bar .nav-list ul');
 const menu_item = document.querySelectorAll('.header .nav-bar .nav-list ul li a');
 const header = document.querySelector('.header.container');
 
-hamburger.addEventListener('click', () => {
-	hamburger.classList.toggle('active');
+headerList.addEventListener('click', () => {
+	headerList.classList.toggle('active');
 	mobile_menu.classList.toggle('active');
 });
 
@@ -19,7 +19,7 @@ document.addEventListener('scroll', () => {
 
 menu_item.forEach((item) => {
 	item.addEventListener('click', () => {
-		hamburger.classList.toggle('active');
+		headerList.classList.toggle('active');
 		mobile_menu.classList.toggle('active');
 	});
 });
--- footer.php
+++ footer.php
@@ -2,7 +2,7 @@
 	  <section id="footer">
 		<div class="footer container">
 		  <div class="brand"><h1><span>S</span>wiss <span>L</span>earns</h1></div>
-		  <h2>Your Complete Web Solution</h2>
+		  <h2>La solution absolue d'apprentissage</h2>
 		  <div class="social-icon">
 			<div class="social-item">
 			  <a href="#"><img src="https://img.icons8.com/bubbles/100/000000/facebook-new.png"/></a>
